340bda9bfccedd1c1a77b59b626cad88846e9b62
MANISHA BOSE <manishabose@MANISHAs-Laptop.local>
2024-08-04 22:21:55 +0530

    Define template for how it should respond.

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate #allows for easy interaction with LLMs
+
+#template for how it should respond to queries
+
+template = """"
+Answer the question below.
+Here is the conversation history: {context}
+Question: {question}
+Answer: 
+"""
 
 model = OllamaLLM(model="llama3.1")
+prompt = ChatPromptTemplate.from_template(template)
+#chain the model and prompt together using langchain
+chain = prompt | model
 
-result = model.invoke(input="Hey! What's up?")
+result = chain.invoke({"context": "", "question": "How good are you?"})
 print(result)
